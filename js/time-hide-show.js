3df5a652ace187fc8b193e9d0606c0f497fb7098
suny555 <suny.w68@gmail.com>
2021-06-29 10:01:42 +0600

    s

--- js/time-hide-show.js
+++ js/time-hide-show.js
@@ -9,7 +9,7 @@
             const d = now.getDay()
         
 
-        if ((d === 2) && (n > '00:00:00' && n < '11:15:00')) {
+        if ((d === 2) && (n > '00:00:00' && n < '10:00:00')) {
                 $('.exam-1 .class-will-start').hide();
                 $('.exam-2 .class-will-start').hide();
                 $('.exam-3 .class-will-start').show();
