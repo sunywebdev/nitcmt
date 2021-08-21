cf4fe6db62be52c2909092165877e87e5e2ffb37
sunywebdev <suny.w68@gmail.com>
2021-08-21 17:12:24 +0600

    d

--- js/login.js
+++ js/login.js
@@ -4,8 +4,11 @@ document.getElementById("submit-btn").addEventListener("click", function () {
     if (userId.length < 5 || password.length < 5) {
         document.getElementById("valid-warning").style.display = "block";
         document.getElementById("wrong-warning").style.display = "none";
-    } else if (userId == "computer" && password == "1234567") {
-        window.location.href = "form.html";
+    } else if (userId.toLowerCase() == "computer" && password.toLowerCase() == "1234567") {
+         window.location.href = "form.html";
+        document.getElementById("wrong-warning").style.display = "none";
+        document.getElementById("valid-warning").style.display = "none";
+
     } else {
         document.getElementById("wrong-warning").style.display = "block";
         document.getElementById("valid-warning").style.display = "none";
