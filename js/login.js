b4d54cefc14daecd00dcc07c9c6364522a5b3412
sunywebdev <suny.w68@gmail.com>
2021-08-23 11:02:49 +0600

    d

--- js/login.js
+++ js/login.js
@@ -1,16 +1,33 @@
 document.getElementById("submit-btn").addEventListener("click", function () {
     const userId = document.getElementById("userId").value;
     const password = document.getElementById("password").value;
-    if (userId.length < 5 || password.length < 5) {
-        document.getElementById("valid-warning").style.display = "block";
-        document.getElementById("wrong-warning").style.display = "none";
-    } else if (userId.toLowerCase().trim() == "computer" && password.trim() == 1234567) {
+    const userIdWrong = document.getElementById("userid-wrong-warning");
+    const passwordWrong = document.getElementById("password-wrong-warning");
+    const emptyWarning = document.getElementById("no-input-warning");
+
+    if (userId.toLowerCase().trim() == "computer" && password.trim() == 1234567) {
         window.location.href = "form.html";
-        document.getElementById("wrong-warning").style.display = "none";
-        document.getElementById("valid-warning").style.display = "none";
+        userIdWrong.style.display = "none";
+        passwordWrong.style.display = "none";
+        emptyWarning.style.display = "none";
+
+    } else if (userId.toLowerCase().trim() != "computer" && password.trim() != 1234567) {
+        userIdWrong.style.display = "none";
+        passwordWrong.style.display = "none";
+        emptyWarning.style.display = "block";
+
+    } else if (userId.toLowerCase().trim() != "computer") {
+        userIdWrong.style.display = "block";
+        passwordWrong.style.display = "none";
+        emptyWarning.style.display = "none";
+    } else if (password.trim() != 1234567) {
+        userIdWrong.style.display = "none";
+        passwordWrong.style.display = "block";
+        emptyWarning.style.display = "none";
 
     } else {
-        document.getElementById("wrong-warning").style.display = "block";
-        document.getElementById("valid-warning").style.display = "none";
+        userIdWrong.style.display = "none";
+        passwordWrong.style.display = "none";
+        emptyWarning.style.display = "block";
     }
 });
